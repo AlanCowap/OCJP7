d69eaa9c333233673d5c113dfa48bc444843adea
Alan Cowap <spam@alancowap.com>
2017-06-12 15:57:55 +0100

    Initial commit

---
+++ OCJP7/src/com/alancowap/ocjp7/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.alancowap.ocjp7;
+
+/**
+ * @author Alan Cowap
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.printf("%s", "Hello Java Professional Programmers");
+
+	}
+
+}
