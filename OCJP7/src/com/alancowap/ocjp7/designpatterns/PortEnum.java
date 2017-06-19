23abaafe9cd651d8262ddbbe81aa0051316cdccd
Alan Cowap <spam@alancowap.com>
2017-06-19 11:09:40 +0100

    Simple implementation of Singleton Design Pattern coded.

--- OCJP7/src/com/alancowap/ocjp7/designpatterns/PortEnum.java
+++ OCJP7/src/com/alancowap/ocjp7/designpatterns/PortEnum.java
@@ -1,13 +1,5 @@
 /**
- * 
- */
-package com.alancowap.ocjp7.designpatterns;
-
-import java.util.Set;
-import java.util.HashSet;
-
-/**
- * Singleton Design Pattern using an Enum (available since Java 5)
+ * Singleton Design Pattern using an enum (available since Java 5)
  * This example keeps track of the available ports on an IP address.
  * So we want a single instance of our Ports.
  * Note this a simple example and is not thread-safe.
@@ -16,25 +8,35 @@ import java.util.HashSet;
  * @author Alan Cowap
  *
  */
+package com.alancowap.ocjp7.designpatterns;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Create a Singleton using an enum
+ */
 public enum PortEnum {
 	INSTANCE;
 	
+	//TODO Use the Port class (see below) rather than an Integer
 	private Set<Integer> availablePorts;	//keep this private to prevent direct access
-	private PortEnum(){
-		//TODO !!!
+	private PortEnum(){						//keep this private to prevent instantiation, (Note: enum constructors can't be called directly)
 		availablePorts = new HashSet<Integer>();
 		availablePorts.add(5060);
 		availablePorts.add(5061);		
-	}
-	
-	public boolean acquirePort(Integer portNumber){
+	}	
+	public boolean acquirePort(Integer portNumber){	//keep this public so people can get a ref to the singleton
 		return availablePorts.remove(portNumber);
 	}
-	
+	public boolean returnPort(Integer portNumber){
+		return availablePorts.add(portNumber);
+	}
 }
 
 /**
  * Simple class to represent a Port on an IP address
+ * You could implement this (rather than use an Integer) however you should override hashCode and equals
  */
 class Port{
 	int mPortNumber;
--- OCJP7/src/com/alancowap/ocjp7/designpatterns/SingletonApp.java
+++ OCJP7/src/com/alancowap/ocjp7/designpatterns/SingletonApp.java
@@ -1,18 +1,34 @@
 /**
- * 
- */
-package com.alancowap.ocjp7.designpatterns;
-
-/**
  * Demo use of Singleton Design pattern
+ * 
  * @author Alan Cowap
  *
  */
-public class SingletonApp {
+package com.alancowap.ocjp7.designpatterns;
 
+public class SingletonApp {
 	public static void main(String[] args) {
 		System.out.println("Demo of Singleton Design Pattern");
-		
+		PortReserver.reservePort(5060);	//Ok
+		PortReserver.reservePort(5060);	//Oops, this should be blocked, port is already reserved
+		PortReserver.reservePort(5061);	//Ok
+		System.out.println("Return port 5060");
+		PortReserver.returnPort(5060);	//Ok
+		PortReserver.reservePort(5060);	//Ok
 	}
+}
 
+//Does this class look like it's part of a DAO Pattern?
+class PortReserver{
+	public static void reservePort(int portNumber){
+		PortEnum ports = PortEnum.INSTANCE;	//get our singleton
+		System.out.println(ports.acquirePort(portNumber));
+	}
+	
+	public static void returnPort(int portNumber){
+		PortEnum ports = PortEnum.INSTANCE;	//get our singleton
+		System.out.println(ports.returnPort(portNumber));
+	}
+	//TODO overload the reservePort method to take a Port as parameter
+	//TODO overload the returnPort method to take a Port as parameter
 }
