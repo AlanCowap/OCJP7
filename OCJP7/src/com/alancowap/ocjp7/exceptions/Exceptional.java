d97d29b49f2bcb6658a163c129750b70e96a365e
Alan Cowap <spam@alancowap.com>
2017-06-19 13:47:41 +0100

    Demo try-with-resources, catch-or-declare, order of execution.

--- OCJP7/src/com/alancowap/ocjp7/exceptions/Exceptional.java
+++ OCJP7/src/com/alancowap/ocjp7/exceptions/Exceptional.java
@@ -1,5 +1,7 @@
 /**
  * Demo exceptions in Java 7
+ * Check the JLS (Java Language Specification) for info on Exceptions:
+ *  - https://docs.oracle.com/javase/specs/jls/se7/html/jls-14.html#jls-14.20
  * try-with-resources
  * 
  * @author Alan Cowap
@@ -15,23 +17,25 @@ import java.io.Reader;
 public class Exceptional {
 
 	public static void main(String[] args){
-		System.out.println("Demo Exceptions");
-		
+		System.out.println("Demo Exceptions");		
 		Exceptional ex = new Exceptional();
 		
 		try {
 			System.out.println("Let's try this..");
 			ex.tryWithRes();
+			System.out.println("You won't see this if an Exception was thrown.");			
 		} catch (IOException e) {
-			System.out.println("I caught something.");
+			System.out.println("You will see this if an Exception was thrown and caught..");
+			//return; //you can call return, but finally will ALWAYS be run (unless you System.exit or pull the power!)
 		}finally{
-			System.out.println("Finally...");			
+			System.out.println("You will always see this finally, Exception thrown or not");			
 		}
-		System.out.println("I'm done trying.");
+		System.out.println("We caught and handled the Exception so our program continued to run.");
 	}
 	
 	private void tryWithRes () throws FileNotFoundException, IOException{
 		//try-with-resources requires neither a catch nor a finally; but it can have either or both.
+		//Note if tryWithRes() doesn't handle the exceptions (which it doesn't) then it must declare them (which it does). #CatchOrDeclare
 		try(Reader r = new FileReader("somefile.txt")){
 			//read a file
 		}//no need to close the FileReader - it's done automagically 
