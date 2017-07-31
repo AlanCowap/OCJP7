ee666ae1519edfa25e43865a1f879dd7e68dba8b
Alan Cowap <spam@alancowap.com>
2017-07-31 11:27:48 +0100

    Multiple Threads & Multiple Runnables

--- OCJP7/src/com/alancowap/ocjp7/threads/ThreadBasics.java
+++ OCJP7/src/com/alancowap/ocjp7/threads/ThreadBasics.java
@@ -12,9 +12,18 @@ public class ThreadBasics {
 	public static void main(String[] args) {
 		System.out.println("Thread Tester");
 //		ThreadBasics app = new ThreadBasics();
-		NumberThread numberThread = new NumberThread(2);
-		Thread t1 = new Thread(numberThread);		
+
+		NumberThread numberThread1 = new NumberThread(1);
+		Thread t1 = new Thread(numberThread1);				
+		NumberThread numberThread2 = new NumberThread(2);
+		Thread t2 = new Thread(numberThread2);		
+		NumberThread numberThread3 = new NumberThread(3);
+		Thread t3 = new Thread(numberThread3);
 		t1.start();
+		t2.start();
+		t3.start();
+		for (int i=0; i < 50; ++i)
+			System.out.print(0);
 	}
 }
 
