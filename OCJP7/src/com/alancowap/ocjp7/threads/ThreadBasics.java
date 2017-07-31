cf62407e97f27de203b364d8bed0fb059761ea53
Alan Cowap <spam@alancowap.com>
2017-07-31 13:23:29 +0100

    Thread.yield() and Priority

--- OCJP7/src/com/alancowap/ocjp7/threads/ThreadBasics.java
+++ OCJP7/src/com/alancowap/ocjp7/threads/ThreadBasics.java
@@ -7,6 +7,7 @@
  *  - - depends where the sleep() goes, but it can be unreliable, and is very slow.
  *  - Thread.sleep is a static method, it puts the current Thread to sleep
  *  - - you can't put another Thread to sleep, so t2.sleep() puts you to sleep - not t2
+ *  - Maybe Thread.yield() will do the trick for us? Nope, yield sends a RUNNING thread to RUNNABLE
  *  - Note the problem is these are the wrong tools to sequence/choreograph our Threads
  * 
  * 
@@ -18,45 +19,53 @@ package com.alancowap.ocjp7.threads;
 public class ThreadBasics {
 	public static void main(String[] args) {
 		System.out.println("Thread Tester");
-		NumberThread numberThread1 = new NumberThread(1);
+		NumberThread numberThread1 = new NumberThread(1, '\\');
 		Thread t1 = new Thread(numberThread1);				
-		NumberThread numberThread2 = new NumberThread(2);
+		NumberThread numberThread2 = new NumberThread(2, '|');
 		Thread t2 = new Thread(numberThread2);		
-		NumberThread numberThread3 = new NumberThread(3);
+		NumberThread numberThread3 = new NumberThread(3, '/');
 		Thread t3 = new Thread(numberThread3);
-//		//Will setting Priority change the order of execution? Not reliably.
-//		t1.setPriority(Thread.MIN_PRIORITY);
-//		t2.setPriority(Thread.MIN_PRIORITY);
-//		t3.setPriority(Thread.MAX_PRIORITY);
-//		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+		//Will setting Priority change the order of execution? Not reliably.
+		//setPriority() isn't suitable for sequencing
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t2.setPriority(Thread.MIN_PRIORITY);
+		t3.setPriority(Thread.MAX_PRIORITY);
+		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 	
 		t1.start();
 		t2.start();
 		t3.start();
 		for (int i=0; i < 50; ++i){
 			System.out.print(0);
+			Thread.yield();
+			System.out.print("-"); //Let's see when we yield
 		}
 	}
 }
 
 class NumberThread implements Runnable{
 	private int mNumber;
+	private char mSymbol;
 	
-	NumberThread(int number){
+	NumberThread(int number, char symbol){
 		mNumber = number;
+		mSymbol = symbol;
 	}
 	
 	@Override
 	public void run() {
-		try {
-			Thread.sleep(50 * mNumber);
-		} catch (InterruptedException e) {				
-//			e.printStackTrace();
-		}
-
+		//Thread.sleep() isn't suitable for sequencing
+//		try {
+//			Thread.sleep(50 * mNumber);
+//		} catch (InterruptedException e) {				
+////			e.printStackTrace();
+//		}
 		for (int i=0; i < 50; ++i){
 			System.out.print(mNumber);
+			//Thread.yield() isn't suitable for sequencing
+			Thread.yield();
+			System.out.print(mSymbol); //Let's see when we yield
 		}
 	}
-	
+
 }
