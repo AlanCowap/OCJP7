0fc663b6528a1d25395636796250ef461569f605
Alan Cowap <spam@alancowap.com>
2017-08-14 15:51:59 +0100

    Funky for loop

--- OCJP7/src/com/alancowap/ocjp7/HelloWorld.java
+++ OCJP7/src/com/alancowap/ocjp7/HelloWorld.java
@@ -1,16 +1,23 @@
 /**
+ * for loop - corner case
+ * 
+ * @author Alan Cowap
  * 
  */
 package com.alancowap.ocjp7;
 
 /**
- * @author Alan Cowap
+ * 
  *
  */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.printf("%s", "Hello Java Professional Programmers");
+		
+		//Funky for loop
+		for(int i =0; ; )
+			System.out.println("i");
 
 	}
 
